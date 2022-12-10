b9a53f24192742bc056ab87d5c861ee2dda2e406
Mikhail.kovalevsckij@gmail.com <Mikhail.kovalevsckij@gmail.com>
2022-12-10 22:52:05 +0800

    Задача №2

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Задача № 2
+
+//[a = 5;2;-9; b = 7;10;-3]
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+    if (a > b)
+        Console.WriteLine();
+    else
+        Console.WriteLine();
