9899bdbdd174c33ae581c9dfff2c81d95ff9eec3
Mikhail.kovalevsckij@gmail.com <Mikhail.kovalevsckij@gmail.com>
2022-12-11 11:20:29 +0800

    Task_8

---
+++ Task_8/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача №8
+
+
+Console.WriteLine("Введите число 1: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число 2: ");
+int m = int.Parse(Console.ReadLine());
+for (int i = n; i <= m; i++)
+{
+    if (i % 2 == 0) 
+    {
+        Console.Write(i + " ");
+    }
+}
+Console.ReadLine();
