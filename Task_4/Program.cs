05789573a245992bffb090b7e3c78e9d2879eb9d
Mikhail.kovalevsckij@gmail.com <Mikhail.kovalevsckij@gmail.com>
2022-12-11 10:53:45 +0800

    Task_4

--- Task_4/Program.cs
+++ Task_4/Program.cs
@@ -1,2 +1,13 @@
 ﻿// Задача №4
-
+//a=2,3,7; b=44,5,78; c=22,3,9.
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+    if (a > b)
+        Console.WriteLine();
+    else
+        Console.WriteLine();
+        if (b > c)
+             Console.WriteLine();
+        else
+            Console.WriteLine();
